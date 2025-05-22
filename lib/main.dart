085491806881f649c9b7aa4353d82639a6f68b93
Usman <22jzbcs0205@uetpeshawar.edu.pk>
2025-05-22 10:45:11 +0500

    Full rolling dice by clicking on them

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -17,26 +18,57 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 4;
+  int rightDiceNumber = 4;
+  void changeDiceFace()
+  {
+    setState(() {
+      rightDiceNumber = Random().nextInt(6);
+      leftDiceNumber = Random().nextInt(6)   ;
+      print("Button 1 tapped = $leftDiceNumber");
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      children: [
-        Expanded(
-          flex: 1,
-          child: Image(
-            image: AssetImage('images/img.png'),
-          ),
-          ),
-
-        Expanded(
-          flex: 1,
-
-            child: Image(
-            image: AssetImage('images/img.png'),
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: GestureDetector(
+                // ✅ no `const` here
+                onTap: () {
+                  changeDiceFace();
+                },
+                child: Image(
+                  image: AssetImage('images/img_$leftDiceNumber.png'),
+                ),
+              ),
             ),
+            Expanded(
+              flex: 1,
+              child: GestureDetector(
+                onTap: () {
+                  changeDiceFace();
+                },
+                child: Image(
+                  image: AssetImage('images/img_$rightDiceNumber.png'),
+                ),
+              ),
+            ),
+          ],
         ),
-    ],
+      ),
     );
   }
 }
